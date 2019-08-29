77cb26490ea92f1684ed7d2935cb0c3b15960859
Joey Yang <zeyuyang2022@u.northwestern.edu>
2019-08-29 17:24:50 -0400

    Fixed Comment

--- mitoRequest.py
+++ mitoRequest.py
@@ -7,6 +7,5 @@ dir = sys.argv[1].rstrip('/')
 for filename in os.listdir(dir):
     editedName = dir + '/' + filename
     if ".bam" in filename and ".bai" not in filename:
-        # subprocess.call("python3 GatkScript.py %s" % editedName, shell=True)
-        print("python3 GatkScript.py %s" % editedName)
+        subprocess.call("python3 GatkScript.py %s" % editedName, shell=True)
     print (editedName)
